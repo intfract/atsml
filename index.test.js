e752d2138afd046dbb4dc62f38711acaf1edb33a
Int Fract <110207136+intfract@users.noreply.github.com>
2024-02-27 10:02:50 +0000

    added reactive types

--- index.test.js
+++ index.test.js
@@ -1,1 +1,4 @@
-import { tag, style } from './dist/index'
+import { Reactive, tag, style } from './dist/index.js'
+
+let variable = new Reactive([1, 2, 3])
+variable.subscribe(x => console.log(x))
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,29 @@
-export function build(html: string) {
-  //
+type Subscriber<T> = ((value: T) => any)
+
+export class Reactive<T> {
+  _value: T
+  subscribers: Subscriber<T>[] = []
+  constructor(value: T) {
+    this._value = value
+  }
+
+  subscribe(subscriber: Subscriber<T>): this {
+    this.subscribers.push(subscriber)
+    return this
+  }
+
+  broadcast() {
+    this.subscribers.forEach(subscriber => subscriber(this.value))
+  }
+
+  get value(): T {
+    return this._value
+  }
+
+  set value(newValue: T) {
+    this._value = newValue
+    this.subscribers.forEach(subscriber => subscriber(newValue))
+  }
 }
 
 export function tag(name: string, innerHTML: string, attributes?: Record<string, string>): string {
