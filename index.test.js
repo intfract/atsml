50b97a5aa941b49c73366257622be0b1b00e8e65
Int Fract <110207136+intfract@users.noreply.github.com>
2024-02-28 05:04:36 +0000

    changed testing

--- index.test.js
+++ index.test.js
@@ -1,8 +1,16 @@
 import { Reactive, addChild, button, tag } from './dist/index'
 
 let counter = new Reactive(0)
-const btn = button([counter], {}, e => counter.value++)
+const btn = button(['count: ', counter], {}, e => counter.value++)
 addChild(
   document.body,
-  btn,
+  [
+    tag(
+      'p',
+      [
+        'Click the button to increment its value!'
+      ],
+    ),
+    btn,
+  ],
 )
